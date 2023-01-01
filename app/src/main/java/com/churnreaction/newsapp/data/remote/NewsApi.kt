e68d4f86d83a9605a934ebfd2659e083a6b237d8
Qasaiemh <energy.programmer21@gmail.com>
2023-01-01 22:57:28 +0200

    First lunch app
    adding news app project, for more open read me file

---
+++ app/src/main/java/com/churnreaction/newsapp/data/remote/NewsApi.kt
@@ -0,0 +1,25 @@
+package com.churnreaction.newsapp.data.remote
+
+import com.churnreaction.newsapp.data.model.NewsResponse
+import com.churnreaction.newsapp.util.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country") countryCode: String = "tr",
+        @Query("page") pageNumber: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q") searchQuery: String,
+        @Query("page") pageNumber: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+}
---
+++ app/src/main/java/com/churnreaction/newsapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.churnreaction.newsapp.util
+
+const val API_KEY = "e4f8f4d37c8f460fa8068e30d0397b3a"
+const val BASE_URL = "https://newsapi.org"
+const val SEARCH_NEWS_TIME_DELAY = 500L
+const val QUERY_PAGE_SIZE = 20
